8f6e677f8fffc3c6fb322eaf27f086f905e6330a
eblondel <emmanuel.blondel1@gmail.com>
2026-07-10 09:35:23 +0200

    get rid of data.table dependency in CWP_to_grid_coordinates

--- R/grid_utils.R
+++ R/grid_utils.R
@@ -55,47 +55,63 @@ center_from_grid_code = function(grid_code) {
 #'@export
 #'@examples CWP_to_grid_coordinates("5201123")
 #'@examples CWP_to_grid_coordinates("6205125")
-CWP_to_grid_coordinates = function(grid_code) {
-  s = as.integer(substr(grid_code, 1, 1))
-
-  dx = dy = 1
-
-  if     (s == 6) { dx =     dy =  5 }
-  else if(s == 3) { dx =     dy = 10 }
-  else if(s == 2) { dx = 20; dy = 10 }
-  else if(s == 4) { dx =     dy = 20 }
-  else if(s == 1) { dx =     dy = 30 }
-
-  q =   as.integer(substr(grid_code, 2, 2))
-
-  lat = as.integer(substr(grid_code, 3, 4))
-  lon = as.integer(substr(grid_code, 5, 7))
-
-  points = data.table(POS = character(), LON = numeric(), LAT = numeric())
-
-  if (q == 1) {       #NE quadrant
-    points = rbind(points, list("NW",  lon     ,  lat + dy))
-    points = rbind(points, list("NE",  lon + dx,  lat + dy))
-    points = rbind(points, list("SE",  lon + dx,  lat))
-    points = rbind(points, list("SW",  lon     ,  lat))
-  } else if(q == 2) { #SE quadrant
-    points = rbind(points, list("NW",  lon     , -lat))
-    points = rbind(points, list("NE",  lon + dx, -lat))
-    points = rbind(points, list("SE",  lon + dx, -lat - dy))
-    points = rbind(points, list("SW",  lon     , -lat - dy))
-  } else if(q == 3) { #SW quadrant - doesn't really apply to the IO
-    points = rbind(points, list("NW", -lon - dx, -lat))
-    points = rbind(points, list("NE", -lon     , -lat))
-    points = rbind(points, list("SE", -lon     , -lat - dy))
-    points = rbind(points, list("SW", -lon - dx, -lat - dy))
-  } else if(q == 4) { #NW quadrant - doesn't really apply to the IO
-    points = rbind(points, list("NW", -lon - dx,  lat + dy))
-    points = rbind(points, list("NE", -lon     ,  lat + dy))
-    points = rbind(points, list("SE", -lon     ,  lat))
-    points = rbind(points, list("SW", -lon - dx,  lat))
+CWP_to_grid_coordinates <- function(grid_code) {
+  
+  s <- as.integer(substr(grid_code, 1, 1))
+  
+  dx <- dy <- 1
+  
+  if (s == 6) {
+    dx <- dy <- 5
+  } else if (s == 3) {
+    dx <- dy <- 10
+  } else if (s == 2) {
+    dx <- 20
+    dy <- 10
+  } else if (s == 4) {
+    dx <- dy <- 20
+  } else if (s == 1) {
+    dx <- dy <- 30
   }
-
-  return(points)
+  
+  q   <- as.integer(substr(grid_code, 2, 2))
+  lat <- as.integer(substr(grid_code, 3, 4))
+  lon <- as.integer(substr(grid_code, 5, 7))
+  
+  points <- data.frame(
+    POS = character(),
+    LON = numeric(),
+    LAT = numeric(),
+    stringsAsFactors = FALSE
+  )
+  
+  if (q == 1) {       # NE quadrant
+    points <- rbind(points, data.frame(POS="NW", LON=lon,      LAT=lat+dy))
+    points <- rbind(points, data.frame(POS="NE", LON=lon+dx,   LAT=lat+dy))
+    points <- rbind(points, data.frame(POS="SE", LON=lon+dx,   LAT=lat))
+    points <- rbind(points, data.frame(POS="SW", LON=lon,      LAT=lat))
+    
+  } else if (q == 2) { # SE quadrant
+    points <- rbind(points, data.frame(POS="NW", LON=lon,      LAT=-lat))
+    points <- rbind(points, data.frame(POS="NE", LON=lon+dx,   LAT=-lat))
+    points <- rbind(points, data.frame(POS="SE", LON=lon+dx,   LAT=-lat-dy))
+    points <- rbind(points, data.frame(POS="SW", LON=lon,      LAT=-lat-dy))
+    
+  } else if (q == 3) { # SW quadrant
+    points <- rbind(points, data.frame(POS="NW", LON=-lon-dx,  LAT=-lat))
+    points <- rbind(points, data.frame(POS="NE", LON=-lon,     LAT=-lat))
+    points <- rbind(points, data.frame(POS="SE", LON=-lon,     LAT=-lat-dy))
+    points <- rbind(points, data.frame(POS="SW", LON=-lon-dx,  LAT=-lat-dy))
+    
+  } else if (q == 4) { # NW quadrant
+    points <- rbind(points, data.frame(POS="NW", LON=-lon-dx,  LAT=lat+dy))
+    points <- rbind(points, data.frame(POS="NE", LON=-lon,     LAT=lat+dy))
+    points <- rbind(points, data.frame(POS="SE", LON=-lon,     LAT=lat))
+    points <- rbind(points, data.frame(POS="SW", LON=-lon-dx,  LAT=lat))
+  }
+  
+  rownames(points) <- NULL
+  points
 }
 
 #' @title convert_to_CWP_grid
